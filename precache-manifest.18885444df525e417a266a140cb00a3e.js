33b394c716b678af3533ad80a100fb699482fae3
Richy Chen <richychn@Richys-MacBook-Pro.local>
2020-02-08 20:37:03 -0800

    Updates

---
+++ precache-manifest.18885444df525e417a266a140cb00a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b68a88bea944d1dff6983f8dd50d04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3dae266c837744abef9",
+    "url": "/static/css/main.dcd2cbae.chunk.css"
+  },
+  {
+    "revision": "a1fbe624b7d745bd5257",
+    "url": "/static/js/2.5827530e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5827530e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3dae266c837744abef9",
+    "url": "/static/js/main.f4de0045.chunk.js"
+  },
+  {
+    "revision": "1b6ea6caa8bd1d81b480",
+    "url": "/static/js/runtime-main.3ab76bb0.js"
+  }
+]);
