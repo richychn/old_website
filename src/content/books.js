a6fbd55e42e3924e713628d3c49e8ee4029a53bb
Richy Chen <richychn@Richys-MacBook-Pro.local>
2020-03-19 14:03:20 -0700

    update books

--- src/content/books.js
+++ src/content/books.js
@@ -10,23 +10,23 @@ export default {
         link_url: "https://www.amazon.com/Titan-Life-John-Rockefeller-Sr/dp/0679438084/ref=sr_1_2?dchild=1&keywords=rockefeller+titan&qid=1584562679&s=books&sr=1-2"
     },
     2: {
+        title: "Hillbilly Elegy: A Memoir of a Family and Culture in Crisis",
+        image_url: "https://images-na.ssl-images-amazon.com/images/I/51gufUMfXZL._SX328_BO1,204,203,200_.jpg",
+        link_url: "https://www.amazon.com/Hillbilly-Elegy-Memoir-Family-Culture/dp/0062300547"
+    },
+    3: {
         title: "The House of Morgan",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/518k5q1%2BjFL._SX331_BO1,204,203,200_.jpg",
         link_url: "https://www.amazon.com/gp/product/0802144659/ref=dbs_a_def_rwt_bibl_vppi_i4"
     },
-    3: {
+    4: {
         title: "Never Split the Difference",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/51yKczFDuQL.jpg",
         link_url: "https://www.amazon.com/Never-Split-Difference-Negotiating-Depended/dp/B01COR1GM2/ref=sr_1_2?dchild=1&keywords=never+split+the+difference&qid=1584562374&sr=8-2"
     },
-    4: {
+    5: {
         title: "Quiet",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/71SLQKwNLBL.jpg",
         link_url: "https://www.amazon.com/Quiet-Power-Introverts-World-Talking/dp/B00714PZMQ/ref=sr_1_1?dchild=1&keywords=quiet&qid=1584562393&s=audible&sr=1-1"
-    },
-    5: {
-        title: "Thinking, Fast and Slow",
-        image_url: "https://images-na.ssl-images-amazon.com/images/I/41RtytNpsfL._SX332_BO1,204,203,200_.jpg",
-        link_url: "https://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman/dp/0374533555"
     },
 }
