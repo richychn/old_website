84893f1b56e3cbce6c5dbcc9e87987a5b3586b01
Richy Chen <richychn@Richys-MacBook-Pro.local>
2020-03-25 22:06:36 -0700

    snowball

--- src/content/books.js
+++ src/content/books.js
@@ -15,31 +15,36 @@ export default {
         link_url: "https://www.amazon.com/Hillbilly-Elegy-Memoir-Family-Culture/dp/0062300547"
     },
     3: {
+        title: "The Snowball: Warren Buffett and the Business of Life",
+        image_url: "https://images-na.ssl-images-amazon.com/images/I/51KVWaAP02L._SX328_BO1,204,203,200_.jpg",
+        link_url: "https://www.amazon.com/Snowball-Warren-Buffett-Business-Life/dp/0553384619"
+    },
+    4: {
         title: "The House of Morgan",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/518k5q1%2BjFL._SX331_BO1,204,203,200_.jpg",
         link_url: "https://www.amazon.com/gp/product/0802144659/ref=dbs_a_def_rwt_bibl_vppi_i4"
     },
-    4: {
+    5: {
         title: "Never Split the Difference",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/51yKczFDuQL.jpg",
         link_url: "https://www.amazon.com/Never-Split-Difference-Negotiating-Depended/dp/B01COR1GM2/ref=sr_1_2?dchild=1&keywords=never+split+the+difference&qid=1584562374&sr=8-2"
     },
-    5: {
+    6: {
         title: "Quiet",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/71SLQKwNLBL.jpg",
         link_url: "https://www.amazon.com/Quiet-Power-Introverts-World-Talking/dp/B00714PZMQ/ref=sr_1_1?dchild=1&keywords=quiet&qid=1584562393&s=audible&sr=1-1"
     },
-    6: {
+    7: {
         title: "Trillion Dollar Coach",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/41vgassjrKL._SX329_BO1,204,203,200_.jpg",
         link_url: "https://www.amazon.com/Trillion-Dollar-Coach-Leadership-Playbook/dp/0062839268"
     },
-    7: {
+    8: {
         title: "The Ride of a Lifetime: Lessons Learned from 15 Years as the CEO of the Walt Disney Company",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/41gr0fllE9L._SX327_BO1,204,203,200_.jpg",
         link_url: "https://www.amazon.com/Ride-Lifetime-Lessons-Learned-Company/dp/0399592091"
     },
-    8: {
+    9: {
         title: "Range: Why Generalists Triumph in a Specialized World",
         image_url: "https://images-na.ssl-images-amazon.com/images/I/41-nFxo5yTL._SX333_BO1,204,203,200_.jpg",
         link_url: "https://www.amazon.com/Range-Generalists-Triumph-Specialized-World/dp/0735214484"
