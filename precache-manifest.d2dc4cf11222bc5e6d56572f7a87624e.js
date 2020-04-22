f9d29e36ab9c18805bdd0cffade61e87ab86ca55
Richy Chen <richychn@Richys-MacBook-Pro.local>
2020-04-21 21:08:34 -0700

    Updates

--- precache-manifest.d2dc4cf11222bc5e6d56572f7a87624e.js
+++ precache-manifest.d2dc4cf11222bc5e6d56572f7a87624e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7629fc779ebeefe4b563a364c9e06b49",
+    "revision": "f34abfefe4ec7cd85d0a49f1fd36a899",
     "url": "/index.html"
   },
   {
-    "revision": "e11477dd4821c19359da",
-    "url": "/static/css/main.64a240fa.chunk.css"
+    "revision": "4e5f85472896b8ee40bc",
+    "url": "/static/css/main.ca505578.chunk.css"
   },
   {
     "revision": "326650cf7ae970f69a61",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3daf95dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e11477dd4821c19359da",
-    "url": "/static/js/main.5cda1f6e.chunk.js"
+    "revision": "4e5f85472896b8ee40bc",
+    "url": "/static/js/main.0cbca559.chunk.js"
   },
   {
     "revision": "e067eb6875a2d0a6534f",
